204b851a9c72a3cf28d9d83dc1a2f5c203ca3d1d
power_840 <liuyu19921017@gmail.com>
2019-09-24 15:43:26 +0800

    v 0.1 1. 实现http服务器 2. 实现get路由请求

--- test/index.js
+++ test/index.js
@@ -3,9 +3,12 @@ const express = require('./../lib/express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.end('Hello World!')
+})
+app.get('/123', function (req, res) {
+  res.end('123!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!')
 })
